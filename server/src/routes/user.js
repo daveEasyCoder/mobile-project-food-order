52a13c38e33534a13f12be70ade44138c847327b
Abdulhafiz redwan <hafizredwan10@gmail.com>
2025-05-11 09:01:36 +0100

    user's route

---
+++ server/src/routes/user.js
@@ -0,0 +1,13 @@
+import express from "express";
+import upload from "../config/multer.js";
+import productHandler from "../controllers/products.controller.js";
+import userController from "../controllers/users.controller.js";
+import authMiddleHandler from "../middlewares/authMiddleware.js";
+
+const user = express.Router();
+
+user.get("/users/me",userController.getProfile);
+user.put("/users/me",upload.single("img"),userController.updateProfile);
+
+
+export default user;
